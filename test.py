8e7cb34344fa8f4d98554209cf8c61faacc88022
Edyfitriyanto12 <88916277+Edyfitriyanto12@users.noreply.github.com>
2025-04-07 21:22:32 +0700

    Update test.py

--- test.py
+++ test.py
@@ -2,18 +2,18 @@ import streamlit as st
 import pandas as pd
 from streamlit_autorefresh import st_autorefresh
 
-# Auto refresh tiap 10 detik
-st_autorefresh(interval=10 * 1000, key="auto_refresh")
-
+# HARUS PALING ATAS setelah import
 st.set_page_config(page_title="Data dari Google Sheet", layout="wide")
+
+# Auto-refresh tiap 10 detik
+st_autorefresh(interval=10 * 1000, key="auto_refresh")
 
 st.title("📄 Data dari Google Spreadsheet (Realtime)")
 
-# URL CSV dari spreadsheet kamu
 spreadsheet_url = "https://docs.google.com/spreadsheets/d/1aFLGmvdviHrPQyKeFcD1jdZU9A3g_RJEMP8X_iMCA7s/export?format=csv"
 
 try:
-    df = pd.read_csv(spreadsheet_url,skiprows=2)
+    df = pd.read_csv(spreadsheet_url)
     st.success("✅ Data berhasil dimuat dan auto-refresh tiap 10 detik.")
     st.dataframe(df, use_container_width=True)
 
